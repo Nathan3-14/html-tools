96522247bbf1886b4b6a0e4d81475736b9d9170c
Nathan3-14 <nathan.watson.172@gmail.com>
2024-03-30 21:01:00 +0000

    Fixed

--- src/yml_to_dict.py
+++ src/yml_to_dict.py
@@ -5,14 +5,39 @@ import yaml
 from rich.console import Console
 
 
-def convert_item(_input: Dict[str, Any]) -> List[dict]:
-    to_return = []
+def convert_item(_input: Dict[str, Any]) -> Dict[str, Any]:
+    to_return = {}
     for property, value in _input.items():
+        try:
+            latest_key = list(to_return.keys())[0]
+        except IndexError:
+            latest_key = None
+
+        console.print(f"{property} = {value}")
+
         match property:
             case "title":
-                to_return.append({"h1": {
-                    
-                }})
+                to_return["h1"] = {
+                    "__text": value
+                }
+            case "subtitle":
+                to_return["h2"] = {
+                    "__text": value
+                }
+            case "image":
+                to_return["img"] = {
+                    "_src": value
+                }
+            case "text":
+                to_return["p"] = {
+                    "__text": value
+                }
+            case "section":
+                to_return["div"] = {
+                    "children": [convert_item(element) for element in value if isinstance(element, dict)]
+                }
+            case "id":
+                to_return[latest_key]["_id"] = value
 
 
     return to_return
@@ -23,15 +48,16 @@ def convert(input_path: str, output_path: str) -> None:
     input_dict_root_key = list(input_dict.keys())[0]
     output_dict = {}
 
-    # console.print_json(json.dumps(input_dict)) #! Debug !#
-
     output_dict[input_dict_root_key] = []
     input_dict = input_dict[input_dict_root_key]
 
+
     for child in input_dict:
-        console.print(child)
+        output_dict[input_dict_root_key].append(convert_item(child))
     
-    console.print_json(json.dumps(output_dict)) #! Debug !#
+    open(output_path, "w").write(yaml.dump(output_dict))
+
+    
 
 
 if __name__ == "__main__":
